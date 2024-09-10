a984a63a74e14b26e3cd688cc0ac222456d42d5a
SunTheCoder <sunenglishjr@gmail.com>
2024-09-10 13:01:56 -0400

    users fix-test

--- backend/db/migrations/20240829114057-Users.js
+++ backend/db/migrations/20240829114057-Users.js
@@ -1,6 +1,12 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn("Users", "firstName", {
@@ -11,7 +17,7 @@ module.exports = {
     await queryInterface.addColumn("Users", "lastName", {
       type: Sequelize.STRING(256),
       allowNull: false,
-    });  
+    }, options);  
   },
 
   async down (queryInterface, Sequelize) {
