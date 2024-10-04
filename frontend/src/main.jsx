99a9222f5b662ba5f071095d4bc4eb882fe5544f
Aaron <offxcxal.aaron@icloud.com>
2024-10-03 23:58:45 -0700

    Phase - 0 completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,20 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 (async () => {
   const store = await configureStore();
 
   // Make the store accessible in the browser console
   window['store'] = store;
+
+  if (import.meta.env.MODE !== 'production') {
+    restoreCSRF();
+  
+    window.csrfFetch = csrfFetch;
+    window.store = store;
+  }
 
   // Render the application once the store is ready
   ReactDOM.createRoot(document.getElementById('root')).render(
