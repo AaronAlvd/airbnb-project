86b87744d49b695f319c8b39f35ccaa0ddb04da1
Aaron <offxcxal.aaron@icloud.com>
2024-10-09 22:28:34 -0700

    Fixed reload problem with signup

--- frontend/src/components/SignUp/SignUpForm.jsx
+++ frontend/src/components/SignUp/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
@@ -9,14 +9,30 @@ import './SignupForm.css';
 function SignupFormModal() {
   const dispatch = useDispatch();
   const [cookies] = useCookies(['XSRF-Token']);
-  const cookie = Object.values(cookies)[0]
+  // const cookie = Object.values(cookies)[0]
   const sessionUser = useSelector((state) => state.session.user);
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const usernameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
+  const refVariables = {
+    firstName: firstNameRef,
+    lastName: lastNameRef,
+    username: usernameRef,
+    email: emailRef,
+    password: passwordRef,
+    confirmPassword: confirmPasswordRef,
+  }
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false); // Loading state
   const [isFocused, setIsFocused] = useState({
@@ -29,13 +45,16 @@ function SignupFormModal() {
   });
   const { closeModal } = useModal();
 
-  // Redirect if the user is already logged in
   if (sessionUser) return <Navigate to="/" replace={true} />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const { password, confirmPassword, email, username, firstName, lastName } = formData;
+
     if (password === confirmPassword) {
       setErrors({});
+      setLoading(true)
       return dispatch(
         sessionActions.signUpUser({
           email,
@@ -43,9 +62,11 @@ function SignupFormModal() {
           firstName,
           lastName,
           password
+        }))
+        .then(() => {
+          closeModal
+          window.location.reload();
         })
-      )
-        .then(closeModal)
         .catch(async (res) => {
           const data = await res.json();
           if (data?.errors) {
@@ -58,75 +79,73 @@ function SignupFormModal() {
     });
   };
 
+  const handleChange = (e) => {
+    return setFormData({
+      ...formData, 
+      [e.target.name]: e.target.value
+    })
+  };
+
   const handleFocus = (e, name) => {
     e.stopPropagation();
-
-    return setIsFocused({  
-      firstName: false,
-      lastName: false,
-      username: false,
-      email: false,
-      password: false,
-      confirmPassword: false, 
-      [name]: true 
-    })
+    refVariables[name].current.focus();
+    setIsFocused({...isFocused, [name]: true })   
   };
 
   const handleBlur = () => {
-    return setIsFocused({
-      firstName: false,
-      lastName: false,
-      username: false,
-      email: false,
-      password: false,
-      confirmPassword: false,
-    })
+    for (let name in isFocused) {
+      if (formData[name] !== "") {
+        setIsFocused(prev => ({...prev, [name]: true }));
+      } else {
+        setIsFocused(prev => ({...prev, [name]: false }));
+      }
+    }
   };
 
   return (
-    <div onClick={() => handleBlur()}>
+    <div>
       <div className="div-h1">
         <h1>Sign Up</h1>
       </div>
       <form className="registerForm" onSubmit={handleSubmit}>
-        <div className="register-div-input" onClick={(e) => {handleFocus(e,'firstName')}}>
+        <div className="register-div-input" tabIndex={0} onBlur={() => handleBlur()} onClick={(e) => {handleFocus(e, "firstName")}}>
           <label className={isFocused.firstName ? "focusedLabel" : "register-formLabel"}>First Name</label>
-          <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} 
-                 className={isFocused.firstName ? "focused-inputBox" : "register-inputBox"} required />
+          <input ref={firstNameRef} type="text" id="firstName" name="firstName" value={formData.firstName} onChange={(e) => handleChange(e)} 
+                 className={isFocused.firstName? "focused-inputBox" : "register-inputBox"} required />
           {errors.firstName && <p>{errors.firstName}</p>}
         </div>
 
-        <div className="register-div-input" onClick={(e) => {handleFocus(e,'lastName')}}>
+        <div className="register-div-input" tabIndex={0} onBlur={() => handleBlur()} onClick={(e) => {handleFocus(e, "lastName")}}>
           <label className={isFocused.lastName ? "focusedLabel" : "register-formLabel"}>Last Name</label>
-          <input type="text" id="lastName" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} 
+          <input ref={lastNameRef} type="text" id="lastName" name="lastName" value={formData.lastName} onChange={(e) => handleChange(e)} 
                  className={isFocused.lastName ? "focused-inputBox" : "register-inputBox"} required />
           {errors.lastName && <p>{errors.lastName}</p>}
         </div>
 
-        <div className="register-div-input" onClick={(e) => {handleFocus(e,'username')}}>
+        <div className="register-div-input" tabIndex={0} onBlur={() => handleBlur()} onClick={(e) => {handleFocus(e, "username")}}>
           <label className={isFocused.username ? "focusedLabel" : "register-formLabel"}>Username</label>
-          <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} 
+          <input ref={usernameRef} type="text" id="username" name="username" value={formData.username} onChange={(e) => handleChange(e)}
                  className={isFocused.username ? "focused-inputBox" : "register-inputBox"} required />
           {errors.username && <p>{errors.username}</p>}
         </div>
 
-        <div className="register-div-input" onClick={(e) => {handleFocus(e,'email')}}>
+        <div className="register-div-input" tabIndex={0} onBlur={() => handleBlur()} onClick={(e) => {handleFocus(e, "email")}}>
           <label className={isFocused.email ? "focusedLabel" : "register-formLabel"}>Email</label>
-          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} 
+          <input ref={emailRef} type="email" id="email" name="email" value={formData.email} onChange={(e) => handleChange(e)} 
             className={isFocused.email ? "focused-inputBox" : "register-inputBox"} required />
           {errors.email && <p>{errors.email}</p>}
         </div>
         
-        <div className="register-div-input" onClick={(e) => {handleFocus(e,'password')}}>
+        <div className="register-div-input" tabIndex={0} onBlur={() => handleBlur()} onClick={(e) => {handleFocus(e, "password")}}>
           <label className={isFocused.password ? "focusedLabel" : "register-formLabel"}>Password</label>
-          <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} 
+          <input ref={passwordRef} type="password" id="password" name="password" value={formData.password} onChange={(e) => handleChange(e)} 
                  className={isFocused.password ? "focused-inputBox" : "register-inputBox"} required />
           {errors.password && <p>{errors.password}</p>}
         </div>
 
-        <div className="register-div-input" onClick={(e) => {handleFocus(e,'confirmPassword')}}>
+        <div className="register-div-input" tabIndex={0} onBlur={() => handleBlur()} onClick={(e) => {handleFocus(e, "confirmPassword")}}>
           <label className={isFocused.confirmPassword ? "focusedLabel" : "register-formLabel"}>Confirm Password</label>
-          <input type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
+          <input ref={confirmPasswordRef} type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={(e) => handleChange(e)}
                  className={isFocused.confirmPassword ? "focused-inputBox" : "register-inputBox"} required />
           {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
         </div>
