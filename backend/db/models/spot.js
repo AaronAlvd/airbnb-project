4dcc4d92e925f56dc00b521d1aa5558cc9cdb8b0
SunTheCoder <sunenglishjr@gmail.com>
2024-10-26 15:25:15 -0400

    spot TEXT migration

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -68,7 +68,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     price: {
