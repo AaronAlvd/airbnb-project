8817db8488f069c4166630346d8e7d130b387137
SunTheCoder <sunenglishjr@gmail.com>
2024-10-26 13:57:41 -0400

    user seeder test

--- backend/db/seeders/20240828032500-demo-user.js
+++ backend/db/seeders/20240828032500-demo-user.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const { User } = require('../models');
 const bcrypt = require("bcryptjs");
 
 let options = {};
@@ -8,11 +7,10 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-/** @type {import('sequelize-cli').Migration} */
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    await queryInterface.bulkInsert(options, [
       {
         id: 1,
         firstName: 'Demo',
